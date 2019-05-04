babe96f301469b309efb6e490fe82be24c91e4c6
lyubomyr83 <lyubomyr83@mail.ru>
2019-05-04 16:44:09 +0300

    switch-case

---
+++ switch.php
@@ -0,0 +1,15 @@
+<?php
+$a=7;
+
+switch ($a)
+{
+    case 5:
+        echo "пять";
+        break;
+    case 6:
+        echo "шесть";
+        break;
+    case 7:
+        echo "семь";
+        break;
+}
